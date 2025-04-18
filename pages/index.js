f153befd30442db88ecdbf2944dcd8580cadbb49
FUTRADERX <asedrew117@gmail.com>
2025-04-17 21:24:50 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,9 @@ export default function Home() {
     <div className="container">
       <h1 className="logo">FUTRADERX</h1>
       <p className="tagline">Get paid to trade. No cap, no fluff.</p>
-      <Link href="/dashboard">
-        <button className="start-btn">Start Challenge</button>
-      </Link>
+      <Link href="/login">
+  <button className="start-btn">Start Challenge</button>
+</Link>
     </div>
   );
 }
